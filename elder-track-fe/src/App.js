acedaab1251655ef2b8aa4fd902ee3467749d715
Mădălina Sandu <sandu.madalina151@gmail.com>
2023-05-07 14:50:39 +0300

    Implement Log In Form

--- elder-track-fe/src/App.js
+++ elder-track-fe/src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import LandingPage from './pages/LandingPage/LandingPage.js'
+import LogIn from './pages/LogIn/LogIn.js'
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route exact path="/" element={<LandingPage/>}/>
+                <Route exact path="/login" element={<LogIn/>}/>
             </Routes>
         </BrowserRouter>
     )
