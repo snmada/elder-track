b7ec5cd1ea2b9aa9d6c7c3117e4de02800e24d27
Mădălina Sandu <sandu.madalina151@gmail.com>
2023-05-09 20:38:03 +0300

    Implement a page for admin to generate user accounts

--- elder-track-fe/src/App.js
+++ elder-track-fe/src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import LandingPage from './pages/LandingPage/LandingPage.js'
 import LogIn from './pages/LogIn/LogIn.js'
 import Dashboard from './pages/Admin/Dashboard/Dashboard.js'
+import CreateUserAccount from './pages/Admin/CreateUserAccount/CreateUserAccount.js'
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Route exact path="/" element={<LandingPage/>}/>
                 <Route exact path="/login" element={<LogIn/>}/>
                 <Route exact path="/dashboard" element={<Dashboard/>}/>
+                <Route exact path="/create-user" element={<CreateUserAccount/>}/>
             </Routes>
         </BrowserRouter>
     )
