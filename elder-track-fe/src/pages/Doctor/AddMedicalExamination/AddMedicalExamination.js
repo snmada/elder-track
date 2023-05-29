4130d02d21ebb6f1c55368646253866880f84d6d
Mădălina Sandu <sandu.madalina151@gmail.com>
2023-05-29 20:49:16 +0300

    Add type field for referrals in AddMedicalExamination.js

--- elder-track-fe/src/pages/Doctor/AddMedicalExamination/AddMedicalExamination.js
+++ elder-track-fe/src/pages/Doctor/AddMedicalExamination/AddMedicalExamination.js
@@ -1,26 +1,47 @@
 import {React, useState} from 'react'
-import {Grid, Typography, Button, TextField, Box, Paper, Divider, FormControl, Select, MenuItem} from '@mui/material'
+import {Grid, Typography, Button, TextField, Box, Paper, Divider, FormControl, Select, MenuItem, InputLabel} from '@mui/material'
 import SaveIcon from '@mui/icons-material/Save'
 import Navbar from '../../../components/Navbar/Navbar.js'
 import './AddMedicalExamination.css'
 
 function AddMedicalExamination() {
 
-    const [data, setData] = useState({purpose: "", symptoms: "", diagnosis: 10, referral: "", prescription : ""});
+    const [data, setData] = useState({purpose: "", symptoms: "", diagnosis: 10, prescription : ""});
 
     const handleChange = (e) => {
         setData({...data, [e.target.name]: e.target.value});
     }
 
+    const [referral, setReferral] = useState([{type: "", description: ""}]);
+
+    const handleAddReferral = () => {
+        setReferral([...referral, {type: "", description: ""}]);
+    };
+    
+    const handleReferralChange = (index, event) => {
+        const {name, value} = event.target;
+        const updatedReferral = [...referral];
+        updatedReferral[index] = {...updatedReferral[index], [name]: value};
+        setReferral(updatedReferral);
+    };
+
+    const handleDeleteReferral = (index) => {
+        const updatedReferral= [...referral];
+        updatedReferral.splice(index, 1);
+        setReferral(updatedReferral);
+    };
+
     const onSubmit = (event) => {
         event.preventDefault();
+        const filteredReferral = referral.filter(entry => entry.type !== "" && entry.description !== "");
+        console.log(filteredReferral);
         console.log(data);
     }
 
   return (
     <>
     <Navbar/>
-    <Grid container className="add-consultation-container">
+    <Grid container className="add-medical-examination-container">
         <Paper elevation={0} className="paper">
             <Grid container>
                 <Grid item xs={12} pb={6}>
@@ -30,7 +51,7 @@ function AddMedicalExamination() {
                     <form onSubmit={onSubmit}>
                         <Paper elevation={5} sx={{padding:'45px 50px'}}>
                             <Grid container>
-                                <Grid item xs={12} mt={2} px={1}>
+                                <Grid item xs={12} mt={2} px={1} pb={2}>
                                     <Typography variant="h6" className="description">Motivul prezentării</Typography>
                                 </Grid>
                                 <Grid item xs={12} p={1}>
@@ -44,7 +65,7 @@ function AddMedicalExamination() {
                                         fullWidth
                                     />
                                 </Grid>
-                                <Grid item xs={12} mt={2} px={1}>
+                                <Grid item xs={12} mt={2} px={1} pb={2}>
                                     <Typography variant="h6" className="description">Simptome</Typography>
                                 </Grid>
                                 <Grid item xs={12} p={1}>
@@ -58,7 +79,7 @@ function AddMedicalExamination() {
                                         fullWidth
                                     />
                                 </Grid>
-                                <Grid item xs={12} mt={2} px={1}>
+                                <Grid item xs={12} mt={2} px={1} pb={2}>
                                     <Typography variant="h6" className="description">Diagnostic</Typography>
                                 </Grid>
                                 <Grid item xs={12} p={1}>
@@ -75,21 +96,7 @@ function AddMedicalExamination() {
                                         </Select>
                                     </FormControl>
                                 </Grid>
-                                <Grid item xs={12} mt={2} px={1}>
-                                    <Typography variant="h6" className="description">Trimiteri</Typography>
-                                </Grid>
-                                <Grid item xs={12} p={1}>
-                                    <TextField
-                                        name="referral"
-                                        onChange={handleChange}
-                                        required
-                                        placeholder="Introduceți aici..."
-                                        multiline
-                                        maxRows={10}
-                                        fullWidth
-                                    />
-                                </Grid>
-                                <Grid item xs={12} mt={2} px={1}>
+                                <Grid item xs={12} mt={2} px={1} pb={2}>
                                     <Typography variant="h6" className="description">Rețetă</Typography>
                                 </Grid>
                                 <Grid item xs={12} p={1}>
@@ -102,6 +109,50 @@ function AddMedicalExamination() {
                                         maxRows={10}
                                         fullWidth
                                     />
+                                </Grid>
+                                <Grid item xs={12} mt={2} px={1} pb={2}>
+                                    <Typography variant="h6" className="description">Trimiteri</Typography>
+                                </Grid>
+                                {referral.map((value, index) => (
+                                    <div key={index} style={{width: '100%'}}>
+                                        <Grid item xs={12} sm={12} p={1}>
+                                            <FormControl>
+                                                <InputLabel id="select-label-type">Tip</InputLabel>
+                                                <Select
+                                                    labelId="select-label-type"
+                                                    name="type"
+                                                    label="Tip"
+                                                    value={value.type}
+                                                    onChange={(event) => handleReferralChange(index, event)}
+                                                    sx={{width: '500px'}}
+                                                >
+                                                    <MenuItem value=""><em>None</em></MenuItem>
+                                                    <MenuItem value={10}>Analize</MenuItem>
+                                                    <MenuItem value={20}>Specialitate</MenuItem>
+                                                    <MenuItem value={30}>Tratament</MenuItem>
+                                                    <MenuItem value={30}>Proceduri</MenuItem>
+                                                </Select>
+                                            </FormControl>
+                                        </Grid>
+                                        <Grid item xs={12} sm={12} p={1}>
+                                            <TextField 
+                                                name="description" 
+                                                type="text"  
+                                                label="Descriere" 
+                                                fullWidth 
+                                                value={value.description}
+                                                onChange={(event) => handleReferralChange(index, event)}
+                                            />
+                                        </Grid>
+                                        <Grid item xs={12} pb={0}>
+                                            <Box sx={{display: 'flex', justifyContent: 'flex-end'}}>
+                                                <Button onClick={() => handleDeleteReferral(index)} sx={{color: 'red'}}>ȘTERGE</Button>
+                                            </Box>
+                                        </Grid>
+                                    </div>
+                                ))}
+                                <Grid item xs={12} pb={6}>
+                                    <Button onClick={handleAddReferral}>+ ADAUGĂ</Button>
                                 </Grid>
                                 <Grid item xs={12} p={1} pt={8}>
                                     <Divider/>
